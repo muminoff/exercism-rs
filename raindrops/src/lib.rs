1d6c79668d538d3a9eec5d491a32256bb82348b9
muminoff <smuminov@gmail.com>
2018-02-26 21:35:11 +0900

    Pass raindrops

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,3 +1,8 @@
 pub fn raindrops(n: usize) -> String {
-    unimplemented!()
+    let mut result: String = String::from("");
+    if n % 3 == 0 { result += "Pling" }
+    if n % 5 == 0 { result += "Plang" }
+    if n % 7 == 0 { result += "Plong" }
+    if result.len() == 0 { result = format!("{}", n) }
+    result
 }
